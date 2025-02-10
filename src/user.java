5f0977fc63c3256774ccc2718590dbca31c044bc
Mateo A <mateo@Mateos-Air.lan>
2025-02-09 19:25:22 -0800

    commited to repo using autogit

--- src/user.java
+++ src/user.java
@@ -3,9 +3,12 @@ import java.io.*;
 
 public class User
 {
+    private static int ID = 0; // static int to represent user ID;
+    
+
     public User()
     {
-        
+        ID+=1;
     }
 
     public 
